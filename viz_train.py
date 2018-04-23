08db0ca91bce837dac7f7ce0707104d0d8d3ea48
ferdavid1 <fespinosa@ucdavis.edu>
2018-04-23 14:36:03 -0700

    small

--- viz_train.py
+++ viz_train.py
@@ -3,12 +3,17 @@ import pandas as pd
 import matplotlib.pyplot as plt 
 from ast import literal_eval
 
-data = pd.read_csv('ImageTopologyDataset.csv')
-labels = data['ImageLabels'].values
-data = data['ImageStructure'].values[:26]
-data = list(map(literal_eval, data))
-for x in range(len(data)):
-	plt.figure()
-	plt.plot(data[x])
-	# plt.show()
-	plt.savefig('Visualizations/morse_funcs/' + 'viz_' + 'picture' + str(x+1) + 'digit' + str(labels[x]) + '.png')
+def plot_morse(data, labels):
+	for x in range(len(data)):
+		plt.figure()
+		plt.plot(data[x])
+		# plt.show()
+		plt.savefig('Visualizations/morse_funcs/' + 'viz_' + 'picture' + str(x+1) + 'digit' + str(labels[x]) + '.png')
+
+
+if __name__ == '__main__':
+	data = pd.read_csv('ImageTopologyDataset.csv')
+	labels = data['ImageLabels'].values
+	data = data['ImageStructure'].values[:51]
+	data = list(map(literal_eval, data))
+	plot_morse(data, labels)
