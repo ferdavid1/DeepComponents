33fadfd343f5020739a7ba6d7c2360e94f058b8e
ferdavid1 <fespinosa@ucdavis.edu>
2018-04-30 12:34:49 -0700

    small

--- main.py
+++ main.py
@@ -24,53 +24,49 @@ def load_data(train=True):
         data = pd.read_csv('ImageTopologyTesting.csv')
         shuff = False
     train_x = data['ImageStructure'].values
-    train_y = Variable(torch.from_numpy(data['ImageLabels'].values), requires_grad=False)
+    train_y = torch.from_numpy(data['ImageLabels'].values)
     train_x = list(map(literal_eval, train_x))
     # upper_lim = max([len(x) for x in train_x])
     upper_lim = 51
-    train_x = list(map(torch.from_numpy, pad_component_arrays(train_x, upper_lim)))
-    train_x = np.array(list(map(Variable, [x.float() for x in train_x])))
+    train_x = torch.from_numpy(pad_component_arrays(train_x, upper_lim))
     train = data_utils.TensorDataset(train_x, train_y)
-
-    train_loader = data.utils.DataLoader(train, batch_size=100, shuffle=shuff)
+    train_loader = data_utils.DataLoader(train, batch_size=100, num_workers=5, shuffle=shuff)
     return train_loader, upper_lim
 
 train, upper_lim = load_data(train=True)
-# D_in, H, D_out = upper_lim, 200, 10 
-# model = torch.nn.Sequential(torch.nn.Linear(D_in, H), torch.nn.ReLU(), torch.nn.Linear(H, 100), torch.nn.ReLU(), torch.nn.Linear(100, D_out))
-# loss = torch.nn.CrossEntropyLoss()
-# learning_rate = 1e-2
-# optim = optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9)
+D_in, H, D_out = upper_lim, 200, 10 
+model = torch.nn.Sequential(torch.nn.Linear(D_in, H), torch.nn.ReLU(), torch.nn.Linear(H, 100), torch.nn.ReLU(), torch.nn.Linear(100, D_out))
+loss = torch.nn.CrossEntropyLoss()
+learning_rate = 1e-2
+optim = optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9)
 
-# # for i in range(2000):
-# for i in range(10):
-#     for ind, rep in enumerate(train_x):
-#         optim.zero_grad()
-#         rep = rep.view(1, upper_lim)
-#         y_pred = model(rep)
-#         lossed = loss(y_pred, train_y[ind])
-#         print(i, lossed.data[0])
-#         lossed.backward()
-#         optim.step()
+for i in range(200):
+    for ind, (reps,labels) in enumerate(train):
+        reps, labels = Variable(reps.float(), requires_grad=False), Variable(labels, requires_grad=False)
+        optim.zero_grad()
+        y_pred = model(reps)
+        lossed = loss(y_pred, labels)
+        if ind%2000 == 0:
+            print(i, lossed.data[0])
+        lossed.backward()
+        optim.step()
 
-# torch.save(model, 'model.pt')
-# print('Done Training')
+torch.save(model, 'model.pt')
+print('Done Training')
 
 # model = torch.load('model.pt')
-# test_x, test_y, ul = load_data(train=False)
-# test_x, test_y = test_x[:100], test_y[:100] # test only the first ten images
+# test, ul = load_data(train=False)
 
-# # correct = 0
-# # total = 0
-# for index, rep in enumerate(test_x):
-#     rep = rep.view(1, ul)
-#     y_pred = model(rep)
-#     y_true = test_y[index]
+# correct = 0
+# total = 0
+# for index, (reps, labels) in enumerate(test):
+#     reps, labels = Variable(reps.float(), requires_grad=False), Variable(labels, requires_grad=False)
+#     y_pred = model(reps)
 #     _, predicted = torch.max(y_pred.data, 1)
-#     print(predicted, y_true)
+#     print(predicted, labels)
 #     print('!!!!!')
-    # total += test_y.size(0)
-    # correct += (predicted == y_true).sum().item()
+#     total += labels.size(0)
+#     correct += (predicted == labels.data).sum()
 
 
 # print('Accuracy of the network on the 100 test images: %d %%' % (100 * correct / total))
